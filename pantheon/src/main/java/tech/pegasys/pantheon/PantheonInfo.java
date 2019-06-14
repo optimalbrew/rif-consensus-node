5d0c391eea12a618c070008ed1ff30b691970e08
Raul Laprida <raul.laprida@gmail.com>
2019-06-14 11:43:39 -0300

    rebranding of main module

--- pantheon/src/main/java/tech/pegasys/pantheon/PantheonInfo.java
+++ pantheon/src/main/java/tech/pegasys/pantheon/PantheonInfo.java
@@ -4,7 +4,7 @@ import tech.pegasys.pantheon.util.PlatformDetector;
 
 // This file is generated via a gradle task and should not be edited directly.
 public final class PantheonInfo {
-  private static final String CLIENT_IDENTITY = "pantheon";
+  private static final String CLIENT_IDENTITY = "rsk-private";
   private static final String VERSION =
       CLIENT_IDENTITY
           + "/v"
